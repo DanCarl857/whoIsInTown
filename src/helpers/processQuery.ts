9ebdb1c9efd4f4d75fef3df6427e457db3ba71c4
Daniel Carlson <dancarl758@gmail.com>
2023-12-01 15:11:48 +0100

    update artist name processing

--- src/helpers/processQuery.ts
+++ src/helpers/processQuery.ts
@@ -1,4 +1,6 @@
 export const processQuery = (query: string): string => {
-    console.log(query)
-    return ''
+    let p = query.replace('/', '%252F')
+    let q = p.replace('?', '%253F')
+    let r = q.replace('\"', '%27C')
+    return r
 }
